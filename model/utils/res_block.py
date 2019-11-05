231f879e59278b85a66fe8e8383dca3230847c20
burnpiro <kemalpiro@gmail.com>
2019-11-05 21:37:00 +0100

    reformat

--- model/utils/res_block.py
+++ model/utils/res_block.py
@@ -1,5 +1,6 @@
 from model.utils.convolutional import convolutional
 
+
 def res_block(input_layer, input_channel, filter_num1, filter_num2):
     short_cut = input_layer
     conv = convolutional(input_layer, filters_shape=(1, 1, input_channel, filter_num1))
