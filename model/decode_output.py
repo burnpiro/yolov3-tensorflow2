d51b71fb6bbf3ce4d99f1fed025064a7a5bd6e99
burnpiro <kemalpiro@gmail.com>
2019-11-08 22:40:18 +0100

    decoder

---
+++ model/decode_output.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import numpy as np
+from config import cfg
+from utils import read_class_names, get_anchors
+
+NUM_CLASS = len(read_class_names(cfg.YOLO.CLASSES))
+ANCHORS = get_anchors(cfg.YOLO.ANCHORS)
+STRIDES = np.array(cfg.YOLO.STRIDES)
+
+
+def decode(conv_output, i=0):
+    """
+    [batch_size, output_size, output_size, anchor_per_scale, 5 + num_classes]
+    (x, y, w, h, score, probability)
+    """
+
+    conv_shape = tf.shape(conv_output)
+    batch_size = conv_shape[0]
+    output_size = conv_shape[1]
+
+    conv_output = tf.reshape(conv_output, (batch_size, output_size, output_size, 3, 5 + NUM_CLASS))
+
+    conv_raw_dxdy = conv_output[:, :, :, :, 0:2]
+    conv_raw_dwdh = conv_output[:, :, :, :, 2:4]
+    conv_raw_conf = conv_output[:, :, :, :, 4:5]
+    conv_raw_prob = conv_output[:, :, :, :, 5:]
+
+    y = tf.tile(tf.range(output_size, dtype=tf.int32)[:, tf.newaxis], [1, output_size])
+    x = tf.tile(tf.range(output_size, dtype=tf.int32)[tf.newaxis, :], [output_size, 1])
+
+    xy_grid = tf.concat([x[:, :, tf.newaxis], y[:, :, tf.newaxis]], axis=-1)
+    xy_grid = tf.tile(xy_grid[tf.newaxis, :, :, tf.newaxis, :], [batch_size, 1, 1, 3, 1])
+    xy_grid = tf.cast(xy_grid, tf.float32)
+
+    pred_xy = (tf.sigmoid(conv_raw_dxdy) + xy_grid) * STRIDES[i]
+    pred_wh = (tf.exp(conv_raw_dwdh) * ANCHORS[i]) * STRIDES[i]
+    pred_xywh = tf.concat([pred_xy, pred_wh], axis=-1)
+
+    pred_conf = tf.sigmoid(conv_raw_conf)
+    pred_prob = tf.sigmoid(conv_raw_prob)
+
+    return tf.concat([pred_xywh, pred_conf, pred_prob], axis=-1)
